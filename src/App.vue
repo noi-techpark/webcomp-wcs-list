5f9c091efb2219f6ca64fe5b7e7b6abb7b6fa01d
henri <henri@moosbauer.com>
2023-07-14 13:27:44 +0200

    make reuse compliant

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,9 @@
+<!--
+SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
 <template>
   <body data-bs-theme=light :style="`font-family: '${fontName}'`">
     <div class="container-fluid py-4" :class="`${noPadding ? 'p-0' : ''}`">
