53b194feb3e2421997909fb2f921ebf930d0230e
henri <henri@moosbauer.com>
2023-07-17 16:46:00 +0200

    add spdx header

--- src/components/IconExternal.vue
+++ src/components/IconExternal.vue
@@ -1,3 +1,9 @@
+<!--
+SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+
+SPDX-License-Identifier: AGPL-3.0-or-later
+-->
+
 <template>
   <svg :width="size" :height="size" viewBox="0 0 16 16" stroke="currentColor" fill="none" xmlns="http://www.w3.org/2000/svg">
     <g clip-path="url(#clip0_709_7628)">
